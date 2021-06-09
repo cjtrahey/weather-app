0e21f6a6ceeaafb2910c0fd828b51b6cc4201755
cjtrahey <trahey.03@gmail.com>
2021-06-09 16:52:28 -0400

    added js pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var citySearch
+var cityConditions
+var uvIndex
+var weatherConditions
+var cityHistory
+
+//Search for a city
+function citySearch () {
+
+}
+
+//City name, date, weather icon, temp, himidity, windspeed, and UV index
+function cityConditions () {
+
+}
+
+//Changes color depending on favorable, moderate or severe
+function uvIndex() {
+
+}
+
+//5-day forcast, displays date, weather icon, temp, windpeed, and humidity
+function weatherConditions () {
+
+}
+
+//Adds searched cities and places it in a history cache, clicking brings back to that datapoint
+function cityhistory () {
+localStorage()
+}
+
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
