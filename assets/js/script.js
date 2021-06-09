e62a3759448f731b63d98d4b344d205c66f021e2
cjtrahey <trahey.03@gmail.com>
2021-06-09 17:46:20 -0400

    Added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,21 @@
-var citySearch
-var cityConditions
-var uvIndex
-var weatherConditions
-var cityHistory
+var citySearch= document.querySelector('.citySearch');          //Declare values
+var main= document.querySelector('#title');
+var temperature= document.querySelector('.temperature');
+var description= document.querySelector('.description');
+var sky= document.querySelector('.sky');
+var button= document.querySelector('.submit');
 
-//Search for a city
-function citySearch () {
+button.addEventListener('click', function(citySearch) {          //Function to display data for the input city
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
+    .then(response => response.json())
+    .then(data => {
+        var temperatureValue= data['main']['temperature'];
+        var citySearchValue= data['citySearch'];;
+        var descriptionValue= data['weather'][0]['description'];
 
-}
-
-//City name, date, weather icon, temp, himidity, windspeed, and UV index
-function cityConditions () {
-
-}
-
-//Changes color depending on favorable, moderate or severe
-function uvIndex() {
-
-}
-
-//5-day forcast, displays date, weather icon, temp, windpeed, and humidity
-function weatherConditions () {
-
-}
-
-//Adds searched cities and places it in a history cache, clicking brings back to that datapoint
-function cityhistory () {
-localStorage()
-}
-
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+        main.innerHTML = citySearchValue;
+        description.innerHTML= "Description - "+descriptionValue;
+        temp.innerHTML = "Temperature - "+temperatureValue;
+        input.value="";
+    })
+})
